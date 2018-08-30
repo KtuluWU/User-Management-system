e0f66f4be1c86942f35d5e015b5e01834b0c0675
Unknown <wangyuxiang9132@gmail.com>
2018-08-30 18:42:45 +0200

    Add Edit_Product feature
    Add Edit_Product feature

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -8,7 +8,6 @@ use App\Entity\Product;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ProductType;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 
 /**
@@ -19,71 +18,61 @@ class ProductController extends AbstractController
     /**
      * @Route("/", name="ProductPage")
      */
-    public function index()
+    public function index(Request $request)
     {
-        // TODO: 应该查找一页的数量0-20，点击下一页查找20-40？
-        $repository = $this->getDoctrine()->getRepository(Product::class);
-        $products = $repository->findAll();
-        return $this->render('product/index.html.twig', ['products' => $products]);
-    }
-
-    /**
-     * @Route("/register", name="AddNewProduct")
-     */
-    public function saveToDatabase(Request $request){
         $product= new Product();
-
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $product_id = $product->getProductId();
-            $product_name = $product->getProductName();
-            $barcode = $product->getBarcode();
+            $data = $form->getData();
+            $product_manager = $this->getDoctrine()->getManager();
+            $product_id = $this->product_id_generator();
+            $product->setProductId($product_id);
+            $product->setProductName($data->getProductName());
+            $product->setBarcode($data->getBarcode());
             $image = $form->get('image_path')->getData();
             $image_path = md5(uniqid()).'.'.$image->guessExtension();
-            $category = $product->getCategory();
-            $shelf_life = $product->getShelfLife();
-            $promotion = $product->getPromotion();
-            $stock = $product->getStock();
-            $description = $product->getDescription();
-
-            $product_manager = $this->getDoctrine()->getManager();
-            $product_confirmation = $product_manager->getRepository(Product::class)->findBy(array('product_id' => $product_id));
-
-            if (null == $product_confirmation) {
-                $product->setProductId($product_id);
-                $product->setProductName($product_name);
-                $product->setBarcode($barcode);
-                $product->setImagePath($image_path);
-                $product->setCategory($category);
-
-                $product->setShelfLife($shelf_life);
-                $product->setPromotion($promotion);
-                $product->setStock($stock);
-                $product->setDescription($description);
-                $product_manager->persist($product);
-                $product_manager->flush();
-                $image->move(
-                    $this->getParameter('upload_directory'),
-                    $image_path
-                );
-
-            }
-
-            else {
-                return new Response("product exists!!!");
-            }
+            $product->setImagePath($image_path);
+            $product->setCategory($data->getCategory());
+            $product->setShelfLife($data->getShelfLife());
+            $product->setPromotion($data->getPromotion());
+            $product->setStock($data->getStock());
+            $product->setDescription($data->getDescription());
+            $product_manager->persist($product);
+            $product_manager->flush();
+            $image->move(
+                $this->getParameter('upload_directory'),
+                $image_path
+            );
+            unset($purchase);
+            unset($form);
+            $purchase = new Product();
+            $form = $this->createForm(ProductType::class, $purchase);
+            $repository = $this->getDoctrine()->getRepository(Product::class);
+            $products = $repository->findAll();
+            $this->product_id_setter($product_id);
+            return $this->render('product/index.html.twig',
+                ['products' => $products,
+                'form'=> $form->createView()]);
         }
 
-        return $this->render('product/product_register.html.twig', [
-            'form' => $form->createView()
-        ]);
+
+
+
+
+
+        $repository = $this->getDoctrine()->getRepository(Product::class);
+        $products = $repository->findAll();
+        return $this->render('product/index.html.twig',
+            ['products' => $products,
+                'form'=> $form->createView()]);
     }
+
     /**
      * @Route("/delete/{product_id}&{page}", name = "ProductEdit")
      */
-    public function userList_delete($product_id, $page)
+    public function product_delete($product_id, $page)
     {   $product_manager = $this->getDoctrine()->getManager();
         $product= $product_manager->getRepository(Product::class)->findOneBy(array('product_id' => $product_id));
         if ($product != null){
@@ -93,5 +82,101 @@ class ProductController extends AbstractController
 
         return $this->redirectToRoute("ProductProductPage");
     }
+    /**
+     * @Route("/edit/{product_id}&{page}")
+     */
+    public function product_edit(Request $request, $product_id, $page)
+    {
+        $product_manager = $this->getDoctrine()->getManager();
+        $product= $product_manager->getRepository(Product::class)->findOneBy(array('product_id' => $product_id));
+        $product_form = new Product();
+        $form = $this->createForm(ProductType::class,$product_form);
+        $form->get('product_name')->setData($product->getProductName());
+        $form->get('barcode')->setData($product->getBarcode());
+        $form->get('category')->setData($product->getCategory());
+        $form->get('shelf_life')->setData($product->getShelfLife());
+        $form->get('promotion')->setData($product->getPromotion());
+        $form->get('stock')->setData($product->getStock());
+        $form->get('description')->setData($product->getDescription());
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $product->setProductName($data->getProductName());
+            $product->setBarcode($data->getBarcode());
+            $product->setCategory($data->getCategory());
+            $product->setShelfLife($data->getShelfLife());
+            $product->setPromotion($data->getPromotion());
+            $product->setStock($data->getStock());
+            $product->setDescription($data->getDescription());
 
+
+            //save new image
+            $image_file = $form->get('image_path')->getData();
+            $new_image_path = md5(uniqid()).'.'.$image_file->guessExtension();
+            $image_file->move(
+                $this->getParameter('upload_directory'),
+                $new_image_path
+            );
+            //delete original image and save new image
+            $image_path = $product->getImagePath();
+            $original_image = $this->getParameter('upload_directory')."/".$image_path;
+            if (file_exists($original_image)) {
+                unlink($original_image);
+            }
+            $product->setImagePath($new_image_path);
+            //update product
+            $product_manager->persist($product);
+            $product_manager->flush();
+
+            return $this->redirectToRoute('ProductProductPage');
+            }
+
+        return $this->render('product/product_edit.html.twig', [
+            'form' => $form->createView(),
+            'page' => $page,
+            'image_path' => $product->getImagePath()
+        ]);
+
+    }
+
+
+    private function product_id_generator()
+    {
+        $col_id_name = 'product_id';
+
+        $em_users = $this->getDoctrine()->getManager()->getConnection();
+        $sql = "SELECT $col_id_name FROM infos ORDER BY $col_id_name DESC LIMIT 1";
+
+        $id_pre = $em_users->prepare($sql);
+        $id_pre->execute();
+        $id_last = $id_pre->fetchAll();
+
+        if ( empty($id_last) || ($id_last[0][$col_id_name])=="")
+        {
+            $product_id = '1000000001';
+        } else {
+            $product_id = (int)$id_last[0][$col_id_name] + 1;
+        }
+        return (string)$product_id;
+    }
+    private function product_id_setter($product_id)
+    {
+        $em_users = $this->getDoctrine()->getManager()->getConnection();
+
+        $sql_init = "SELECT * FROM infos LIMIT 1";
+        $stm_init = $em_users->prepare($sql_init);
+        $stm_init->execute();
+        $db = $stm_init->fetchAll();
+
+        if (empty($db)) {
+            $sql_first = "INSERT INTO infos VALUES (0,'','','',0,0,0,'','','')";
+            $stm_first = $em_users->prepare($sql_first);
+            $stm_first->execute();
+        }
+        $col_id_name = 'product_id';
+
+        $sql = "UPDATE infos SET $col_id_name = $product_id ";
+        $stm = $em_users->prepare($sql);
+        $stm->execute();
+    }
 }
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -13,47 +13,50 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('product_id', null, array(
-                'attr' => array(
-                    'placeholder' => 'product_page.product_id',
-                ),
-                'translation_domain' => 'ums'))
             ->add('product_name', null, array(
+                'label' => 'product_page.product_name',
                 'attr' => array(
                     'placeholder' => 'product_page.product_name',
                 ),
                 'translation_domain' => 'ums'))
             ->add('barcode', null, array(
+                'label' => 'product_page.barcode',
                 'attr' => array(
                     'placeholder' => 'product_page.barcode',
                 ),
                 'translation_domain' => 'ums'))
             ->add('image_path', FileType::class, array(
+                'label' => 'product_page.image_path',
                 'attr' => array(
                     'placeholder' => 'product_page.image_path',
                 ),
                 'translation_domain' => 'ums'))
             ->add('category', null, array(
+                'label' => 'product_page.category',
                 'attr' => array(
                     'placeholder' => 'product_page.category',
                 ),
                 'translation_domain' => 'ums'))
             ->add('shelf_life', null, array(
+                'label' => 'product_page.shelf_life',
                 'attr' => array(
                     'placeholder' => 'product_page.shelf_life',
                 ),
                 'translation_domain' => 'ums'))
             ->add('promotion', null, array(
+                'label' => 'product_page.promotion',
                 'attr' => array(
                     'placeholder' => 'product_page.promotion',
                 ),
                 'translation_domain' => 'ums'))
             ->add('stock', NumberType::class, array(
+                'label' => 'product_page.stock',
                 'attr' => array(
                     'placeholder' => 'product_page.stock',
                 ),
                 'translation_domain' => 'ums'))
             ->add('description', TextType::class, array(
+                'label' => 'product_page.description',
                 'attr' => array(
                     'placeholder' => 'product_page.description',
                 ),
